f7c85e0e2250fc84f94d429da615ff7204042eaa
Shubhanil Roy <shubhanil@tecions.com>
2020-03-26 11:11:43 +0530

    Print Layout Complete with Admin Login Layout

--- resources/views/component/header.blade.php
+++ resources/views/component/header.blade.php
@@ -86,10 +86,12 @@
                 <!-- Messages -->
                 <!-- ============================================================== -->
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark" href="" id="2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="mdi mdi-email"></i>
+                    {{--
+                    <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark"
+                       href="" id="2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="mdi mdi-email"></i>
                         <div class="notify"> <span class="heartbit"></span> <span class="point"></span> </div>
-                    </a>
-                    <div class="dropdown-menu mailbox animated slideInUp" aria-labelledby="2">
+                    </a>--}}
+                   {{-- <div class="dropdown-menu mailbox animated slideInUp" aria-labelledby="2">
                         <ul>
                             <li>
                                 <div class="drop-title">You have 4 new messages</div>
@@ -126,7 +128,7 @@
                                 <a class="nav-link text-center" href="javascript:void(0);"> <strong>See all e-Mails</strong> <i class="fa fa-angle-right"></i> </a>
                             </li>
                         </ul>
-                    </div>
+                    </div>--}}
                 </li>
                 <!-- ============================================================== -->
                 <!-- End Messages -->
@@ -134,7 +136,7 @@
                 <!-- ============================================================== -->
                 <!-- Messages -->
                 <!-- ============================================================== -->
-                <li class="nav-item dropdown mega-dropdown"> <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="mdi mdi-view-grid"></i></a>
+                {{--<li class="nav-item dropdown mega-dropdown"> <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="mdi mdi-view-grid"></i></a>
                     <div class="dropdown-menu animated slideInUp">
                         <ul class="mega-dropdown-menu row">
                             <li class="col-lg-3 col-xlg-2 m-b-30">
@@ -223,7 +225,7 @@
                             </li>
                         </ul>
                     </div>
-                </li>
+                </li>--}}
                 <!-- ============================================================== -->
                 <!-- End Messages -->
                 <!-- ============================================================== -->
--- resources/views/pages/datewiseProductPrint.blade.php
+++ resources/views/pages/datewiseProductPrint.blade.php
@@ -1,12 +1,12 @@
 @extends('base')
 @section('content')
 
-    <link href="{{asset('assets/plugins/bootstrap-datepicker/bootstrap-datepicker.min.css')}}" rel="stylesheet"
-          type="text/css"/>
+    <!-- Bootstrap Core CSS -->
+    <link href="{{asset('assets/plugins/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/plugins/bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css')}}" rel="stylesheet">
 
-    <!-- Page plugins css -->
-    <link href="{{asset('assets/plugins/bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css')}}"
-          rel="stylesheet">
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 
     <div class="page-wrapper">
         <!-- ============================================================== -->
@@ -52,18 +52,20 @@
                                         <div class="example">
                                             <h5 class="box-title m-t-30">Starting Date</h5>
                                             <div class="input-group">
-                                                <input type="date" class="form-control" id="datepicker-autoclose"
+                                                <input type="text" class="form-control" id="mdate"
                                                        placeholder="dd-MM-yyyy" name="start_date" >
-                                                <span class="input-group-addon"><i class="icon-calender"></i></span> </div>
+                                                <span class="input-group-addon"><i class="icon-calender"></i></span>
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="example">
                                             <h5 class="box-title m-t-30">Ending Date</h5>
-                                           <div class="input-group">
-                                                <input type="date" class="form-control" id="datepicker-autoclose-2"
+                                            <div class="input-group">
+                                                <input type="text" class="form-control" id="mdate-2"
                                                        placeholder="dd-MM-yyyy" name="end_date" >
-                                                <span class="input-group-addon"><i class="icon-calender"></i></span> </div>
+                                                <span class="input-group-addon"><i class="icon-calender"></i></span>
+                                            </div>
                                         </div>
                                     </div>
 
@@ -90,162 +92,109 @@
 
     </div>
 
-    <script src="{{asset('assets/plugins/switchery/dist/switchery.min.js')}}"></script>
-    <script src="{{asset('assets/plugins/select2/dist/js/select2.full.min.js')}}" type="text/javascript"></script>
-    <script src="{{asset('assets/plugins/bootstrap-select/bootstrap-select.min.js')}}" type="text/javascript"></script>
-    <script src="{{asset('assets/plugins/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js')}}"></script>
-    <script src="{{asset('assets/plugins/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.js')}}"
-            type="text/javascript"></script>
-    <script type="text/javascript" src="{{asset('assets/plugins/multiselect/js/jquery.multi-select.js')}}"></script>
-    <script>
-        jQuery(document).ready(function () {
-            // Switchery
-            var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
-            $('.js-switch').each(function () {
-                new Switchery($(this)[0], $(this).data());
-            });
-            // For select 2
-            $(".select2").select2();
-            $('.selectpicker').selectpicker();
-            //Bootstrap-TouchSpin
-            $(".vertical-spin").TouchSpin({
-                max:10000,
-                verticalbuttons: true,
-                verticalupclass: 'ti-plus',
-                verticaldownclass: 'ti-minus'
-            });
-            var vspinTrue = $(".vertical-spin").TouchSpin({
-                verticalbuttons: true
-            });
-            if (vspinTrue) {
-                $('.vertical-spin').prev('.bootstrap-touchspin-prefix').remove();
-            }
-            $("input[name='gst_per']").TouchSpin({
-                min: 0,
-                max: 100,
-                step: 0.1,
-                decimals: 2,
-                boostat: 5,
-                maxboostedstep: 10,
-                postfix: '%'
-            });
-            $("input[name='disc_per']").TouchSpin({
-                min: 0,
-                max: 100,
-                step: 0.1,
-                decimals: 2,
-                boostat: 5,
-                maxboostedstep: 10,
-                postfix: '%'
-            });
-            $("input[name='price']").TouchSpin({
-                min: 1,
-                max: 1000000000,
-                stepinterval: 50,
-                maxboostedstep: 10000000,
-                prefix: '₹'
-            });
-            $("input[name='qty'][type='text']").TouchSpin();
-            $("input[name='tch3_22']").TouchSpin({
-                initval: 40
-            });
-            $("input[name='tch5']").TouchSpin({
-                prefix: "pre",
-                postfix: "post"
-            });
-            // For multiselect
-            $('#pre-selected-options').multiSelect();
-            $('#optgroup').multiSelect({
-                selectableOptgroup: true
-            });
-            $('#public-methods').multiSelect();
-            $('#select-all').click(function () {
-                $('#public-methods').multiSelect('select_all');
-                return false;
-            });
-            $('#deselect-all').click(function () {
-                $('#public-methods').multiSelect('deselect_all');
-                return false;
-            });
-            $('#refresh').on('click', function () {
-                $('#public-methods').multiSelect('refresh');
-                return false;
-            });
-            $('#add-option').on('click', function () {
-                $('#public-methods').multiSelect('addOption', {
-                    value: 42,
-                    text: 'test 42',
-                    index: 0
-                });
-                return false;
-            });
-            $(".ajax").select2({
-                ajax: {
-                    url: "https://api.github.com/search/repositories",
-                    dataType: 'json',
-                    delay: 250,
-                    data: function (params) {
-                        return {
-                            q: params.term, // search term
-                            page: params.page
-                        };
-                    },
-                    processResults: function (data, params) {
-                        // parse the results into the format expected by Select2
-                        // since we are using custom formatting functions we do not need to
-                        // alter the remote JSON data, except to indicate that infinite
-                        // scrolling can be used
-                        params.page = params.page || 1;
-                        return {
-                            results: data.items,
-                            pagination: {
-                                more: (params.page * 30) < data.total_count
-                            }
-                        };
-                    },
-                    cache: true
-                },
-                escapeMarkup: function (markup) {
-                    return markup;
-                }, // let our custom formatter work
-                minimumInputLength: 1,
-                templateResult: formatRepo, // omitted for brevity, see the source of this page
-                templateSelection: formatRepoSelection // omitted for brevity, see the source of this page
-            });
-        });
-    </script>
-    <script src="{{asset('assets/plugins/calendar/dist/lib/jquery-ui.min.js')}}"></script>
+
+    <script src="{{asset('assets/plugins/jquery/jquery.min.js')}}"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="{{asset('assets/plugins/bootstrap/js/popper.min.js')}}"></script>
+
+    <script src="{{asset('assets/plugins/bootstrap/js/bootstrap.min.js')}}"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="{{asset('js/jquery.slimscroll.js')}}"></script>
+    <!--Wave Effects -->
+    <script src="{{asset('js/waves.js')}}"></script>
+    <!--Menu sidebar -->
+    <script src="{{asset('js/sidebarmenu.js')}}"></script>
+    <!--stickey kit -->
+    <script src="{{asset('assets/plugins/sticky-kit-master/dist/sticky-kit.min.js')}}"></script>
+    <script src="{{asset('assets/plugins/sparkline/jquery.sparkline.min.js')}}"></script>
+    <!--Custom JavaScript -->
+    <script src="{{asset('js/custom.min.js')}}"></script>
+    <!-- ============================================================== -->
+    <!-- Plugins for this page -->
+    <!-- ============================================================== -->
+    <!-- Plugin JavaScript -->
+
     <script src="{{asset('assets/plugins/moment/moment.js')}}"></script>
-    <script
-        src="{{asset('assets/plugins/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js')}}"></script>
-    <!-- Clock Plugin JavaScript -->
-    <script src="{{asset('assets/plugins/clockpicker/dist/jquery-clockpicker.min.js')}}"></script>
-    <!-- Color Picker Plugin JavaScript -->
-    <script src="{{asset('assets/plugins/jquery-asColorPicker-master/libs/jquery-asColor.js')}}"></script>
-    <script src="{{asset('assets/plugins/jquery-asColorPicker-master/libs/jquery-asGradient.js')}}"></script>
-    <script src="{{asset('assets/plugins/jquery-asColorPicker-master/dist/jquery-asColorPicker.min.js')}}"></script>
+    <script src="{{asset('assets/plugins/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js')}}"></script>
     <!-- Date Picker Plugin JavaScript -->
     <script src="{{asset('assets/plugins/bootstrap-datepicker/bootstrap-datepicker.min.js')}}"></script>
-    <!-- Date range Plugin JavaScript -->
-    <script src="{{asset('assets/plugins/timepicker/bootstrap-timepicker.min.js')}}"></script>
-    <script src="{{asset('assets/plugins/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
+
     <script>
         // MAterial Date picker
         $('#mdate').bootstrapMaterialDatePicker({ weekStart : 0, time: false });
+        $('#mdate-2').bootstrapMaterialDatePicker({ weekStart : 0, time: false });
         $('#timepicker').bootstrapMaterialDatePicker({ format : 'HH:mm', time: true, date: false });
         $('#date-format').bootstrapMaterialDatePicker({ format : 'dddd DD MMMM YYYY - HH:mm' });
 
         $('#min-date').bootstrapMaterialDatePicker({ format : 'DD/MM/YYYY HH:mm', minDate : new Date() });
+        // Clock pickers
+        $('#single-input').clockpicker({
+            placement: 'bottom',
+            align: 'left',
+            autoclose: true,
+            'default': 'now'
+        });
+        $('.clockpicker').clockpicker({
+            donetext: 'Done',
+        }).find('input').change(function() {
+            console.log(this.value);
+        });
+        $('#check-minutes').click(function(e) {
+            // Have to stop propagation here
+            e.stopPropagation();
+            input.clockpicker('show').clockpicker('toggleView', 'minutes');
+        });
+        if (/mobile/i.test(navigator.userAgent)) {
+            $('input').prop('readOnly', true);
+        }
+        // Colorpicker
+        $(".colorpicker").asColorPicker();
+        $(".complex-colorpicker").asColorPicker({
+            mode: 'complex'
+        });
+        $(".gradient-colorpicker").asColorPicker({
+            mode: 'gradient'
+        });
+        // Date Picker
+        jQuery('.mydatepicker, #datepicker').datepicker();
+        jQuery('#datepicker-autoclose').datepicker({
+            autoclose: true,
+            todayHighlight: true
+        });
+        jQuery('#date-range').datepicker({
+            toggleActive: true
+        });
+        jQuery('#datepicker-inline').datepicker({
+            todayHighlight: true
+        });
+        // Daterange picker
+        $('.input-daterange-datepicker').daterangepicker({
+            buttonClasses: ['btn', 'btn-sm'],
+            applyClass: 'btn-danger',
+            cancelClass: 'btn-inverse'
+        });
+        $('.input-daterange-timepicker').daterangepicker({
+            timePicker: true,
+            format: 'MM/DD/YYYY h:mm A',
+            timePickerIncrement: 30,
+            timePicker12Hour: true,
+            timePickerSeconds: false,
+            buttonClasses: ['btn', 'btn-sm'],
+            applyClass: 'btn-danger',
+            cancelClass: 'btn-inverse'
+        });
+        $('.input-limit-datepicker').daterangepicker({
+            format: 'MM/DD/YYYY',
+            minDate: '06/01/2015',
+            maxDate: '06/30/2015',
+            buttonClasses: ['btn', 'btn-sm'],
+            applyClass: 'btn-danger',
+            cancelClass: 'btn-inverse',
+            dateLimit: {
+                days: 6
+            }
+        });
     </script>
 
-    <!-- <script src="assets/plugins/moment/min/moment.min.js"></script> -->
-    <script src="{{asset('assets/plugins/wizard/jquery.steps.min.js')}}"></script>
-    <script src="{{asset('assets/plugins/wizard/jquery.validate.min.js')}}"></script>
-    <!-- Sweet-Alert  -->
-    <script src="{{asset('assets/plugins/sweetalert/sweetalert.min.js')}}"></script>
-    <script src="{{asset('assets/plugins/wizard/steps.js')}}"></script>
-    <!-- Typehead Plugin JavaScript -->
-    <script src="{{asset('assets/plugins/typeahead.js-master/dist/typeahead.bundle.min.js')}}"></script>
-    <script src="{{asset('assets/plugins/typeahead.js-master/dist/typeahead-init.js')}}"></script>
 
 @stop
